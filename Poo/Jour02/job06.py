d0f57adfe5ee17fb7e2b9132fb28d2d0d2938b89
Abundancia <abundancia@airdeabundancia.home>
2023-04-25 21:48:20 +0200

    add model portes a la classe voiture qui herite du vehicule

--- Poo/Jour02/job06.py
+++ Poo/Jour02/job06.py
@@ -5,8 +5,18 @@ class Vehicule():
         self.prix = prix
         
     def informationDuVehicule(self, model):
-        self.model = model
-        return f"{self.marque} {self.model} {self.anne} {self.prix}"
+        #self.model = model
+        pass
+    
+class Voiture(Vehicule):
+        def __init__(self, marque, model, anne, prix, portes):
+             super().__init__(marque, anne, prix)
+             self.model = model
+             self.portes = portes
+             
+        def informationDuVehicule(self):
+            return f"{self.marque} {self.model} {self.anne} {self.prix} {self.portes}"
+             
         
-vehicule = Vehicule("Mercedes", 2020, 18500)
-print(vehicule.informationDuVehicule("classe A"))
+voiture = Voiture("Mercedes","classe A", 2020, 18500, 4)
+print(voiture.informationDuVehicule())
